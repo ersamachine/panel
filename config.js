be2233997d2941db3ff3f8b34c39b87dd087ca49
ersamachine <easyboxmachine@gmail.com>
2026-06-27 08:52:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// ERSA Personel Paneli — backend (Apps Script) adresi
 window.API_URL = "https://script.google.com/macros/s/AKfycby_HLxtsoerxFv61SpKOwYR-NFQ8PHjmitVFgUyfsGgTvZZixFzAQvfFOoMO8uLwWDB/exec";
