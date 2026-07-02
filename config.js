5f69bc6140aae0e45dca366c5860868796b467f8
ersamachine <easyboxmachine@gmail.com>
2026-07-02 17:35:37 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.API_URL = "https://script.google.com/macros/s/AKfycby_HLxtsoerxFv61SpKOwYR-NFQ8PHjmitVFgUyfsGgTvZZixFzAQvfFOoMO8uLwWDB/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbxVGRFz6wx_sDcIuVYRGabdS3_7Ky8SeQKez9IfTd7vtsCzGcfn2ONAgtOi3XkqES9K/exec";
